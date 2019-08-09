555a982991cfbafb932e43c262640b3b43e7e4e4
Nick Tuttle <drmrnick87@gmail.com>
2019-08-09 17:29:56 -0400

    added some javascirpt/Jquery
    Started to make things be clickable and added math.random to generate numbers

--- script.js
+++ script.js
@@ -1,2 +1,61 @@
-alert("fuckyou");
-alert("fuckoff");
+
+
+
+//vars
+var blue = 0;
+var green = 0;
+var purple = 0;
+var red = 0;
+
+var currentScore = 0;
+var targetScore = 0;
+
+var wins = 0;
+var losses = 0;
+
+
+//get random number
+
+var randomNumberBetween0and120 = Math.floor(Math.random() * 121);
+
+function randomWholeNum() {
+
+  return Math.random();
+}
+
+//game starting
+
+
+function gameStart = function(){
+
+ var currentScore = 0;
+
+ var targetScore = Math.floor(Math.random() * (120 - 19 + 1));
+
+ blue.valueOf = random(1, 12);
+ green.valueOf = random(1, 12);
+ purple.valueOf = random(1, 12);
+ red.valueOf = random(1, 12);
+
+
+}
+
+
+
+//make rupees click
+
+$("#blue").click(function() {
+    
+})
+
+$("#red").click(function() {
+    
+})
+
+$("#purple").click(function() {
+    
+})
+
+$("#green").click(function() {
+    
+})
